923d2a3a2c071cb8e0f6aff24ccd69917e8ed694
seak03 <ben.teucher1@gmail.com>
2022-05-04 18:39:59 +0200

    println hinzugefügt

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hallo");
         System.out.println("Tschüss");
+        System.out.println("Bye");
     }
 }
