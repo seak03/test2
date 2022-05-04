bf3fb0c50f50c55d7e7dea744f3064aebda91229
Pascal0811 <schoene.pascal.psn@gmail.com>
2022-05-04 18:28:54 +0200

    commit2 Pascal

--- src/Second.java
+++ src/Second.java
@@ -1,7 +1,8 @@
 package src;
 
-public class second {
+public class Second {
     public void second() {
         System.out.println("zweite klasse");
+        System.err.println("Es ist kein Fehler aufgetreten!!");
     }
 }
